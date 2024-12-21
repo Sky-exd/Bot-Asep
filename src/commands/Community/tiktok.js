2583fcb8fcc2ea9f42c132699bf6104f689fa576
kinkenkan298 <akin@programmer.net>
2024-12-21 13:07:34 +0700

    fix(tiktok): benerin bug dikit di tiktok

--- src/commands/Community/tiktok.js
+++ src/commands/Community/tiktok.js
@@ -4,26 +4,15 @@ import {
   ApplicationCommandOptionType,
 } from "discord.js";
 import { Downloader } from "@tobyg74/tiktok-api-dl";
-import fs from "fs";
-import path from "path";
+import { statSync, existsSync, unlinkSync } from "fs";
+import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 import embedbase from "../../utils/embeds.js";
-import create from "../../utils/embeds.js";
-import { finished, Readable } from "stream";
+import downloadFile from "../../utils/downloadFile.js";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const tempDir = path.join(__dirname, "../../../temp");
-
-const downloadFile = async (url, fileName) => {
-  const response = await fetch(url)
-  const tujuanFile = path.join(tempDir, fileName)
-  const fileStream = fs.createWriteStream(tujuanFile, { flags: "w" })
-  finished(Readable.fromWeb(response.body).pipe(fileStream))
-  fileStream.on('error', () => {
-    throw Error('Gagal Download Bang!')
-  })
-}
+const __dirname = dirname(__filename);
+const tempDir = join(__dirname, "../../../temp");
 
 /** @type {import('commandkit').CommandData} */
 export const data = {
@@ -41,118 +30,93 @@ export const data = {
 };
 /** @param {import('commandkit').SlashCommandProps} param0 */
 export async function run({ interaction }) {
-  try {
-    const urlTikok = interaction.options.getString("url");
-    const tiktokDownloader = await Downloader(urlTikok, { version: "v2" });
-    if (!interaction.deferred && !interaction.replied)
-      await interaction.deferReply();
-    if (tiktokDownloader.status === "error") {
-      return interaction.editReply({
-        embeds: [
-          embedbase({
-            type: "error",
-            message: "Link Tiktok ada yang salah bang !",
-          }),
-        ],
-      });
-    }
-    if (typeof tiktokDownloader.result === "undefined") {
-      return interaction.editReply({
-        embeds: [
-          embedbase({
-            type: "error",
-            message: "Gagal download tiktok nya bang !",
-          }),
-        ],
-      });
-    }
-    switch (tiktokDownloader.result.type) {
-      case "video": {
-        const nameFileTemp = `tiktok${Math.ceil(Math.random() * 5000)}_temp.mp4`;
-        const tempFile = path.join(tempDir, nameFileTemp);
-        try {
-          await downloadFile(
-            tiktokDownloader.result.video,
-            nameFileTemp,
-          );
-          const size = fs.statSync(tempFile).size
-          if (size >= 100 * 1024 * 1024 || size >= 25 * 1024 * 1024) {
-            return await interaction.editReply({
-              embeds: [create({
-                type: "error",
-                message: "File terlalu besar bang !"
-              })]
-            })
-          }
-          try {
-            const tiktokVideo = new AttachmentBuilder(tempFile, {
-              name: "tiktok-video.mp4",
-            });
-            return await interaction.editReply({
-              files: [tiktokVideo],
-            });
-          } catch (error) {
-            console.error(error);
-            await interaction.editReply({
-              embeds: [
-                embedbase({
-                  type: "error",
-                  message: "Tiktok Gagal Di Kirim Bang! Coba lagi nanti!",
-                }),
-              ],
-            });
-          }
-        } catch (err) {
-          console.error(err);
-          return await interaction
-            .editReply({
-              embeds: [
-                embedbase({
-                  type: "error",
-                  message: "Ada yang salah sama download file nya",
-                }),
-              ],
-            })
-        } finally {
-          setTimeout(() => {
-            if (fs.existsSync(tempFile)) fs.unlinkSync(tempFile);
-          }, 3000);
-        }
-        break;
-      }
-      case "image": {
-        await interaction.editReply({
-          embeds: [
-            embedbase({
-              type: "info",
-              message: "tiktok gambar belom di dukung bang!",
-            })
-          ]
-        });
-        break;
-      }
-      default:
-        return await interaction.editReply({
-          embeds: [
-            embedbase({
+  if (!interaction.deferred && !interaction.replied)
+    await interaction.deferReply();
+  const urlTikok = interaction.options.getString("url");
+  const tiktokDownloader = await Downloader(urlTikok, { version: "v2" });
+  if (tiktokDownloader.status === "error" || typeof tiktokDownloader.result === "undefined") {
+    return interaction.editReply({
+      embeds: [
+        embedbase({
+          type: "error",
+          message: "Link Tiktok ada yang salah bang !",
+        }),
+      ],
+    });
+  }
+  switch (tiktokDownloader.result.type) {
+    case "video": {
+      const nameFileTemp = `tiktok${Math.ceil(Math.random() * 5000)}_temp.mp4`;
+      const tempFile = join(tempDir, nameFileTemp);
+      try {
+        await downloadFile(
+          tiktokDownloader.result.video,
+          tempFile,
+        );
+        const size = statSync(tempFile).size
+        if (size >= 100 * 1024 * 1024 || size >= 25 * 1024 * 1024 || size < 9) {
+          return await interaction.editReply({
+            embeds: [embedbase({
               type: "error",
-              message: "Tipe Video Tidak Ditemukan bang !!",
-            }),
-          ],
-        });
+              message: "File nya terlalu besar tidak bisa dikirim!!"
+            })]
+          })
+        }
+        try {
+          const tiktokVideo = new AttachmentBuilder(tempFile, {
+            name: "tiktok-video.mp4",
+          });
+          return await interaction.editReply({
+            files: [tiktokVideo],
+          });
+        } catch (error) {
+          console.error(error);
+          await interaction.editReply({
+            embeds: [
+              embedbase({
+                type: "error",
+                message: "Tiktok Gagal Di Kirim Bang! Coba lagi nanti!",
+              }),
+            ],
+          });
+        }
+      } catch (err) {
+        console.error(err);
+        return await interaction
+          .editReply({
+            embeds: [
+              embedbase({
+                type: "error",
+                message: "Ada yang salah sama download file nya",
+              }),
+            ],
+          })
+      } finally {
+        setTimeout(() => {
+          if (existsSync(tempFile)) unlinkSync(tempFile);
+        }, 3000);
+      }
+      break;
     }
-  } catch (error) {
-    console.error("Error handling interaction:", error);
-    if (!interaction.replied) {
+    case "image": {
+      await interaction.editReply({
+        embeds: [
+          embedbase({
+            type: "info",
+            message: "tiktok gambar belom di dukung bang!",
+          })
+        ]
+      });
+      break;
+    }
+    default:
       return await interaction.editReply({
         embeds: [
           embedbase({
             type: "error",
-            message:
-              "Terjadi kesalahan saat memproses permintaan.",
+            message: "Tipe Video Tidak Ditemukan bang !!",
           }),
-        ]
+        ],
       });
-    }
   }
 };
---
+++ src/utils/downloadFile.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { createWriteStream } from 'fs';
+
+const downloadFile = async (urlVideo, dest) => {
+  let file = createWriteStream(dest);
+  let resp = await axios({
+    method: "GET",
+    url: urlVideo,
+    responseType: "stream",
+  });
+
+  return new Promise((resolve, reject) => {
+    var responseSent = false;
+    resp.data.pipe(file);
+    file.on("finish", () => {
+      file.on("error", (err) => {
+        reject(err);
+      });
+      file.on("close", () => {
+        if (responseSent) return;
+        responseSent = true;
+        resolve();
+      });
+    });
+  });
+};
+
+export default downloadFile;
